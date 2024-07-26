9c5ca3c16457152cb863cb47a94753c79422f65f
eddie.j <@value6033>
2024-07-26 09:26:15 +0900

    update : 파일 다운로드 API 추가

---
+++ portal/src/main/java/com/kep/portal/service/download/DownloadService.java
@@ -0,0 +1,21 @@
+package com.kep.portal.service.download;
+
+import com.kep.core.model.dto.download.DownloadDto;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+@Service
+public class DownloadService {
+
+    public File getFile (DownloadDto downloadDto){
+        File file = new File(downloadDto.getFilePath(), downloadDto.getFileName());
+        return file;
+    }
+
+    public static Resource getFileSystemResource(File file) {
+        return new FileSystemResource(file);
+    }
+}
