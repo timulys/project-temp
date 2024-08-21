8c9076b675250399da898131d7cde9a54d118287
eddie.j <@value6033>
2024-08-21 09:37:41 +0900

    update : 메세지 검수 완료 되어 테스트를 위해서 주석 해제

--- portal/src/main/java/com/kep/portal/service/channel/ChannelEnvService.java
+++ portal/src/main/java/com/kep/portal/service/channel/ChannelEnvService.java
@@ -347,8 +347,7 @@ public class ChannelEnvService {
 			return new ChannelEnvDto();
 		}
 
-		// Todo 현재 서버에서 메세지를 제대로 받아오지 못해서 임시 주석처리 ( 일단 내부 DB에 있는 데이터 사용 )
-		/*
+		// 2024.08.21 eddie.j : 메세지 등록 후 승인처리 완료 주석 해제
 		List<IssuePayload> issuePayloads = systemMessageService.getSystemMessage(channel.getServiceKey());
 		if(!ObjectUtils.isEmpty(issuePayloads)){
 			Map<String , IssuePayload> chapter = systemMessageService.setSystemMessage(issuePayloads);
@@ -358,7 +357,6 @@ public class ChannelEnvService {
 			entity.getStart().getWaiting().setMessage(chapter.get("S4"));
 			entity.setImpossibleMessage(chapter.get("S3"));
 		}
-		*/
 
 		return channelEnvMapper.map(entity);
 	}
