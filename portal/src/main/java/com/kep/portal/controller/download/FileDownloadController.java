aa22b6f84c731926cf2b524d3fe8c04b73dce41d
tim.c <tim.c@dktechin.com>
2024-10-17 13:37:25 +0900

    KICA-193 파일 다운로드 기능 수정(최종)

--- portal/src/main/java/com/kep/portal/controller/download/FileDownloadController.java
+++ portal/src/main/java/com/kep/portal/controller/download/FileDownloadController.java
@@ -4,10 +4,9 @@ import com.kep.portal.model.dto.download.DownloadDto;
 import com.kep.portal.service.download.DownloadService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springdoc.api.annotations.ParameterObject;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -16,16 +15,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.file.Files;
 
 @Tag(name = "파일 다운로드 API", description = "/api/v1/download")
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/download")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class FileDownloadController {
 
     private final DownloadService downloadService;
@@ -33,20 +30,20 @@ public class FileDownloadController {
     @Tag(name = "파일 다운로드 API")
     @Operation(summary = "파일 다운로드")
     @GetMapping
-    ResponseEntity<Resource> download(@ParameterObject DownloadDto downloadDto) {
+    public ResponseEntity<? super Resource> download(@ParameterObject @Valid DownloadDto downloadDto) {
 
         File file = downloadService.getFile(downloadDto);
         if (!file.exists()) {
             return ResponseEntity.notFound().build();
         }
-
         Resource resource = downloadService.getFileSystemResource(file);
+
+        // Attachment header information and file name
         String contentDisposition = "attachment; filename=\"" + file.getName() + "\"";
-        return ResponseEntity.ok()
+
+        return ResponseEntity.status(HttpStatus.OK)
                 .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
                 .body(resource);
-
-//        return new ResponseEntity<>(resource, HttpStatus.OK);
     }
 
 }
