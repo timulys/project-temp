12082fa26a3bb6a45675f346ed1da295d6a35597
tim.c <tim.c@dktechin.com>
2025-02-13 17:20:58 +0900

    serviceKey -> senderKey

--- platform/src/main/java/com/kep/platform/model/dto/KakaoCounselReceiveEvent.java
+++ platform/src/main/java/com/kep/platform/model/dto/KakaoCounselReceiveEvent.java
@@ -33,8 +33,8 @@ public class KakaoCounselReceiveEvent {
 	 */
 	@NotEmpty
 	@Schema(description = "메시지를 수신한 발신프로필 키")
-	private String serviceKey;
-//	private String senderKey;
+//	private String serviceKey;
+	private String senderKey;
 
 	// ////////////////////////////////////////////////////////////////////////
 	// Message Event Only
