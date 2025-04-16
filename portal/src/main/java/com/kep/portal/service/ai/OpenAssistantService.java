eb6e719399f66ce5f67c1c948dacbe4c2b86bee5
tim.c <tim.c@dktechin.com>
2025-04-16 15:06:16 +0900

    - AI -> Assistant로 디렉토리명 변경

--- portal/src/main/java/com/kep/portal/service/ai/OpenAssistantService.java
+++
@@ -1,9 +0,0 @@
-package com.kep.portal.service.ai;
-
-import com.kep.portal.model.dto.openai.response.GetChatResponseDto;
-import org.springframework.http.ResponseEntity;
-
-public interface OpenAssistantService {
-    ResponseEntity<? super GetChatResponseDto> findSummaryAiAssistant(Long issueId);
-    ResponseEntity<? super GetChatResponseDto> findAllSummaryAiAssistant(Long guestId);
-}
